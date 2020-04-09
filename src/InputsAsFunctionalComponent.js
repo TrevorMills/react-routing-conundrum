4f77db81d560eb28f2558aa39f8e43a92d454f03
Trevor Mills <trevor@tbkcreative.com>
2020-04-09 09:05:56 -0400

    updating functional as per comment at https://stackoverflow.com/a/61114197/466276

--- src/InputsAsFunctionalComponent.js
+++ src/InputsAsFunctionalComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Redirect } from 'react-router-dom';
 import qs from 'qs';
 import Menu from './Menu';
@@ -16,34 +16,14 @@ import Menu from './Menu';
  */
 const InputsAsFunctionalComponent = props => {
     // Set state with initial values, allowing props.location.search to set those values
-    const [ state, updateState ] = useState(
-        Object.assign(
-            { checkboxes: [], text: '' },
-            qs.parse( props.location.search, { ignoreQueryPrefix: true } )
-        )
+    const state = Object.assign(
+        { checkboxes: [], text: '' },
+        qs.parse( props.location.search, { ignoreQueryPrefix: true } )
     );
-
-    // The activeElement state is my mechanism for remembering what element had focus, but this feels wrong
-    const [ activeElement, updateActiveElement ] = useState( null );
-    const [ applyMechanism, updateApplyMechanism ] = useState( false ); // false means inputs will lose focus, true means we use the mechanism in a useEffect hook
-
-    /**
-     * After the component finishes rendering, we check to see if we have an activeElement, and if we do,
-     * we'll set focus on that element once everything is done.  We put the focus action within a setTimeout so
-     * we only do that once the main thread is done executing.
-     */
-    useEffect( () => {
-        if ( applyMechanism && activeElement ) {
-            const el = document.getElementById( activeElement.id );
-            if ( el ) {
-                el.focus();
-                // If it's a text input, we can put the cursor in the right position
-                if ( typeof activeElement.cursor === 'number' ) {
-                    el.selectionStart = el.selectionEnd = activeElement.cursor;
-                }
-            }
-        }
-    });
+    const updateState = next => {
+        const stringifiedState = '?' + qs.stringify({ ...state, ...next });
+        props.history.replace( props.location.pathname + stringifiedState );
+    };
 
     // Just to get some checkboxes
     const checkboxes = [ 'One', 'Two', 'Three', 'Four' ];
@@ -51,14 +31,7 @@ const InputsAsFunctionalComponent = props => {
     // When text box is changed, update state.text
     const onChangeText = e => {
         const text = e.target.value;
-        updateActiveElement({
-            id: e.target.id,
-            cursor: e.target.selectionStart,
-        });
-        updateState( previous => ({
-            ...previous,
-            text,
-        }));
+        updateState({text});
     };
 
     // When any checkbox is toggled, update state.checkboxes
@@ -71,32 +44,8 @@ const InputsAsFunctionalComponent = props => {
         } else if ( ! e.target.checked && pos !== -1 ) {
             checkboxes.splice( pos, 1 );
         }
-        updateActiveElement({
-            id: e.target.id,
-            cursor: null
-        });
-        updateState( previous => ({
-            ...previous,
-            checkboxes
-        }));
+        updateState({checkboxes});
     };
-
-    // Toggle our "applyMechanism" state
-    const onChangeApplyMechanism = e => {
-        updateActiveElement({
-            id: e.target.id,
-            cursor: null,
-        });
-        updateApplyMechanism( ! applyMechanism );
-    };
-
-    // Here's where the problem arises.  If the state has changed, we want to update the query string on the URL.
-    // The way we're taught is to be declarative and return a <Redirect> component. However, doing that causes this
-    // functional component to fully re-render, so the input that had focus is gone.
-    const stringifiedState = '?' + qs.stringify( state );
-    if ( props.location.search !== stringifiedState ) {
-        return <Redirect push={false} to={props.location.pathname + stringifiedState} />
-    }
 
     return <div>
         <Menu {...props}/>
@@ -112,13 +61,6 @@ const InputsAsFunctionalComponent = props => {
                 {value}
             </label>
         })}
-
-        <blockquote>
-            <label>Apply Mechanism: <input type="checkbox" value={1} checked={applyMechanism} onChange={onChangeApplyMechanism}/></label>
-            <p>There's a mechanism in place to remember the id of the activeElement whenever the state changes. You can toggle
-                whether or not this mechanism is applied.  If it is applied, then the text input retains focus and you can tab through
-                the checkboxes and change them as you go.  If it is NOT applied, you can't do either of these things.</p>
-        </blockquote>
     </div>;
 };
 
